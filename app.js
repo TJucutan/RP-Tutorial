cd8f70eca72e08eb1e9e4db21c33a1af159eb836
TJ <tj.jucutan@inctrg.net>
2021-02-16 15:44:01 -0800

    Users can see all reviews

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Handlebars are Cool!'})
+// })
+
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II"},
+    { title: "Awesome Movie", movieTitle: "Titanic"},
+]
+
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!'})
+    res.render('reviews-index', { reviews: reviews})
 })
 
 app.listen(3000, () => {
